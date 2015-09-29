b0a2989add8db2ba8d7c872f8ea6328055dcd579
Johnsmith0508 <loganwaldman@gmail.com>
2015-09-28 23:45:43 -0400

    starting client basic functoality is there (anyone good at JApplets _help_)

--- src/org/dynalogic/webchat/Client.java
+++ src/org/dynalogic/webchat/Client.java
@@ -1,36 +1,44 @@
 package org.dynalogic.webchat;
-//TODO fix .gitignore
+
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
- * Created by JohnSmith0508 on 28/9/15 at 10:10 PM.
+ * Created by JohnSmith0508 on 28/9/15 at 11:11 PM.
  */
-public class Client extends Frame
+public class Client extends JApplet
 {
+    public Client()
+    {
+        JButton exit = new JButton("Exit");
+        JTextArea outputStream = new JTextArea("hello this is a test made for testing porpuses");
+        JTextField username = new JTextField("Name");
+        JTextField server = new JTextField("logan.waldman.ro");
+
+        outputStream.setColumns(50);
+        outputStream.setRows(35);
+        outputStream.setEditable(false);
+        exit.addActionListener(e -> System.exit(0));
+        server.setHorizontalAlignment(JLabel.LEFT);
+        username.setHorizontalAlignment(JLabel.RIGHT);
+
+        getContentPane().add(username,BorderLayout.PAGE_START);
+        getContentPane().add(server,BorderLayout.PAGE_START);
+        getContentPane().add(outputStream);
+        getContentPane().add(exit, BorderLayout.PAGE_END);
+    }
+    public void init()
+    {
+        JFrame window = new JFrame("Web Chat");
+        Client content = new Client();
+        window.setContentPane(content);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.pack();
+        window.setVisible(true);
+    }
+
     public static void main(String[] args)
     {
-        Client client = new Client();
-    }
-    public Client() throws HeadlessException
-    {
-        init();
-        setSize(300,300);
-        setLayout(null);
-        setVisible(true);
-    }
-    private void init()
-    {
-        Button exit = new Button("Exit");
-        exit.setBounds(0,20,80,30);
-
-        exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        add(exit);
+        new Client().init();
     }
 }
